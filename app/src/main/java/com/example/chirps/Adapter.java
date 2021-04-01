fd29b5354d63fe7b3178efd7d135f18e9628d675
David Sword <vexiconlieus@gmail.com>
2021-04-01 10:40:14 -0700

    Began work on the reminder layout and creating the reminder and recycler view adapter.

---
+++ app/src/main/java/com/example/chirps/Adapter.java
@@ -0,0 +1,51 @@
+package com.example.chirps;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    Context context;
+    ArrayList<Reminder> reminder_list;
+
+    public Adatper(Context c, ArrayList<Reminder> p){
+        context = c;
+        reminder_list = p;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_reminder, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.title.setText(reminder_list.get(position).getTitle());
+        holder.timeT.setText(reminder_list.get(position).getTime());
+        holder.dateT.setText(reminder_list.get(position).getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return reminder_list.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView title, timeT, dateT;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            timeT = (TextView) itemView.findViewById(R.id.time);
+            dateT = (TextView) itemView.findViewById(R.id.date);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chirps/Reminder.java
@@ -0,0 +1,44 @@
+package com.example.chirps;
+
+public class Reminder {
+    String title, date, description, time;
+
+    public Reminder(String title, String date, String description, String time) {
+        this.title = title;
+        this.date = date;
+        this.description = description;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
